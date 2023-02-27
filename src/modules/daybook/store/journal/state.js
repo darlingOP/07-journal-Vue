bb93cb1c66fe449bad46043b96a2e488dc70a58e
darlingOP <darling.o@cliker.mx>
2023-02-27 10:31:53 -0600

    journal app

---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,25 @@
+
+ export default () =>({
+
+    isLoading:true,
+    entries:[
+        {
+            id:new Date().getTime(),
+            date:new Date().toDateString(),
+            text:"Mollit qui voluptate ex eiusmod cupidatat aliquip elit anim mollit. Tempor reprehenderit est sunt ea esse dolor duis magna ad ea velit elit anim. Adipisicing commodo enim magna duis occaecat labore fugiat excepteur nostrud adipisicing. Anim labore non qui fugiat tempor adipisicing cupidatat officia Lorem amet aliqua sint officia reprehenderit. Enim incididunt aliqua Lorem in Lorem. Sunt non pariatur sint irure reprehenderit qui adipisicing in.",
+            picture:null
+        },
+        {
+            id:new Date().getTime() + 1000,
+            date:new Date().toDateString(),
+            text:"Dolore est nostrud consectetur voluptate nisi non reprehenderit ex culpa voluptate consequat velit qui. Quis eu sit aute qui quis eu occaecat ad ad officia. Proident consequat dolore consequat voluptate commodo exercitation sit fugiat laboris consectetur nulla voluptate pariatur amet. Duis consectetur anim consectetur nostrud cupidatat. Nisi amet eu Lorem esse aliqua nisi non ut proident cupidatat eu eiusmod et eiusmod. Lorem veniam officia qui pariatur veniam consectetur Lorem incididunt.",
+            picture:null
+        },
+        {
+            id:new Date().getTime() + 2000,
+            date:new Date().toDateString(),
+            text:"Voluptate reprehenderit pariatur sunt occaecat. Ad duis elit nisi laborum ipsum cupidatat voluptate non reprehenderit duis eu do ut. Eu ipsum sit incididunt culpa. Duis ad culpa occaecat do qui irure quis deserunt voluptate in. Ad exercitation exercitation consequat quis aliqua amet officia laboris reprehenderit aute ipsum elit occaecat.",
+            picture:null
+        },
+    ]
+})
